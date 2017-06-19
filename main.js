506d7bd07a88357fb5422d02760766ead1bc4b7f
Bobby Shunk <Bobby@MacBook-Pro-5.local>
2017-06-19 16:02:30 -0500

    work on seasonal sales

--- main.js
+++ main.js
@@ -4,44 +4,101 @@ let products = null;
 let categories = null;
 // get products
 
+		// inside this loop we need to loop again, but this time through the categories
+		// array to find the one category whos id matches the "category-id" of the 
+		// currentProduct. Maybe a .filter()?
+		// That returned array will contain one object. We can set "Dept" on the new object
+		// we are making with the "name" property of that one object
+
+function buildDOMObj() {
+	// loop through products and categories to grab Prod name, Dept, Price, and cat ID
+	let productArr = products.map( function(currentProduct) {
+		let categoryItem = categories.filter( function(category) {
+			return category.id === currentProduct.category_id;
+		})
+		let prodObj = {dept: categoryItem[0].name}
+		console.log("prod object", prodObj);
+		return prodObj 
+	});
+// For now, just see if map worked
+console.log("prod arr", productArr);
+};
+
 function buildCard(prodObj) {
 	let card = `<div class="prodCard">
 								<h2>${prodObj.name}</h2>
 								<h3>${prodObj.dept}</h3>
 								<p>${prodObj.price}</p>
-							</div>`;
+							</div>`
+							;
 	return card;
 };
 
 let tempObj = {name: "Ferby", dept: "Toys", price: 12.75,}
-console.log("card", buildCard(tempObj)); 
+console.log( "card", buildCard(tempObj) ); 
 
-function displayProducts() {
+function setProducts() {
 	products = JSON.parse(event.target.responseText).products;
+	getCategories();
 };
 
-function displayCategories() {
+function setCategories() {
 	categories = JSON.parse(event.target.responseText).categories;
+	buildDOMObj();
 };
 
 function getCategories() {
 	let reqCategories = new XMLHttpRequest();
-	reqCategories.addEventListener("load", displayCategories);
+	reqCategories.addEventListener("load", setCategories);
 	reqCategories.open("GET", "data/categories.json");	
 	reqCategories.send();
 };
 
 function getProducts() {
 	let reqProducts = new XMLHttpRequest();
-	reqProducts.addEventListener("load", displayProducts);
+	reqProducts.addEventListener("load", setProducts);
 	reqProducts.open("GET", "data/products.json");
 	reqProducts.send();
 };
 getProducts();
-getCategories();
 
 // get categories
 console.log("Products?", products);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
