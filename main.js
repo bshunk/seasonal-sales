a8fafe60c17bac18a2148639adac61cc0e49a575
Bobby Shunk <Bobby@MacBook-Pro-5.local>
2017-06-20 11:01:17 -0500

    work on seasonal sales

--- main.js
+++ main.js
@@ -9,52 +9,84 @@ function displayProducts(productArr) {
 	let cardArr = productArr.map( function(product) {
 		return buildCard(product)
 	})
-	console.log("cardArr ready to go into DOM", cardArr);
+	// console.log("cardArr ready to go into DOM", cardArr);
 	cardArr.forEach( function(card) {
 		let cardWrapper = document.createElement("article");
 		cardWrapper.innerHTML = card;
 		productWrapper.appendChild(cardWrapper);
 	});
-}
-// get products
+};
 
-		// inside this loop we need to loop again, but this time through the categories
-		// array to find the one category whos id matches the "category-id" of the 
-		// currentProduct. Maybe a .filter()?
-		// That returned array will contain one object. We can set "Dept" on the new object
-		// we are making with the "name" property of that one object
+// TODO function to respond to dropdown selection
+document.getElementById("dropdown").addEventListener("change", function() {
+	let selectedSeason = event.target.value
+	console.log("selected Season", selectedSeason);
+	let seasonCategory = categories.filter( function(category) {
+		return category.season_discount.toLowerCase() === selectedSeason.toLowerCase();
+	});
+	let catId = seasonCategory[0].id
+	let prodCards = document.getElementsByClassName("prodCards")
+	for(let i = 0; i < prodCards.length; i++) {
+		
+		if ( parseInt(prodCards[i].getAttribute("data-catId") === catId)) {
+			console.log("product card", prodCards[i]);
+			let pTags = prodCards[i].getElementsByTagName("p");
+			for ( let i = 0; i < pTags.length; i++) {
+				pTags[i].classList.toggle("isHIdden");
+			}
+		}
+	}
+
+})
+
+// grab the value, lowercase it, compare it to the categories data to find the correct discount
+
+// TODO function to change prices in DOM
+
+// inside this loop we need to loop again, but this time through the categories
+// array to find the one category whos id matches the "category-id" of the 
+// currentProduct. Maybe a .filter()?
+// That returned array will contain one object. We can set "Dept" on the new object
+// we are making with the "name" property of that one object
 
 function buildDOMObj() {
-	// loop through products and categories to grab Prod name, Dept, Price, and cat ID
+// loop through products and categories to grab Prod name, Dept, Price, and cat ID
 	let productArr = products.map( function(currentProduct) {
 		let categoryItem = categories.filter( function(category) {
 			return category.id === currentProduct.category_id;
-		})
+		});
 		let prodObj = {
 			dept: categoryItem[0].name,
 			name: currentProduct.name,
 			price: currentProduct.price,
-			catId: currentProduct.category_id
-		}
+			catId: currentProduct.category_id,
+			discountedPrice: calculateDiscountPrice (currentProduct.price, categoryItem[0].discount)
+		};
 		return prodObj
 	});
 // For now, just see if map worked
 console.log("prod arr", productArr);
 displayProducts(productArr)
-}
+};
 
 function buildCard(prodObj) {
-	let card = `<div class="prodCard" data-catId="${prodObj.catId}">
+	let card = `<div class="prodCard" data-catId=${prodObj.catId}>
 								<h2>${prodObj.name}</h2>
 								<h3>${prodObj.dept}</h3>
 								<p>${prodObj.price}</p>
-							</div>`
-							;
+								<p class="isHidden">$${prodObj.discountedPrice}</p>
+							</div>`;
 	return card;
 };
+// Test buildCard
+// let tempObj = {name: "Ferby", dept: "Toys", price: 12.75,}
+// console.log( "card", buildCard(tempObj) ); 
 
-let tempObj = {name: "Ferby", dept: "Toys", price: 12.75,}
-console.log( "card", buildCard(tempObj) ); 
+function calculateDiscountPrice(origPrice, discount) {
+	return +(origPrice * (1.00 - discount)).toFixed(2)
+};
+// Test calculation with dumby data
+console.log("discounted price", calculateDiscountPrice(3.99, .25));
 
 function setProducts() {
 	products = JSON.parse(event.target.responseText).products;
@@ -83,43 +115,3 @@ getProducts();
 
 // get categories
 console.log("Products?", products);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
